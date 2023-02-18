d53ba1ebca72515f38b9354a908570b6c0f0bc4f
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-18 22:23:06 +0100

    Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-    { path: "login", component: LoginComponent },
-    { path: "register", component: RegisterComponent },
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     {
-        path: "",
+        path: '',
         component: DashboardComponent,
-        children: dashboardRoutes
+        children: dashboardRoutes,
+        canActivate: [AuthGuard]
     },
-    { path: "**", redirectTo: "" }
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class AppRoutingModule { }
