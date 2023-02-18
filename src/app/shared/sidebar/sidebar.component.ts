f79d6527dc8b4eece9c18883e9c272ec78a6fb51
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-18 22:04:43 +0100

    Logout

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.authService.logout()
+        .then(() => {
+          this.router.navigate(["/login"]);
+        }).catch(error => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error.message,
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+        });
+  }
 }
