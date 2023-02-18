fe34ffc0d6c04b60980baece927d1cca9b152b11
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-18 22:41:02 +0100

    Crear documento de base de datos por cada usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { map } from 'rxjs/operators';
+import { Usuario } from '../models/usuario.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    public auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) { }
 
   initAuthListener() {
     this.auth.authState.subscribe(fuser => {
@@ -21,7 +26,11 @@ export class AuthService {
 
   crearUsuario(nombre: string, correo: string, password: string) {
     console.log(nombre, correo, password);
-    return this.auth.signInWithEmailAndPassword(correo, password);
+    return this.auth.signInWithEmailAndPassword(correo, password)
+      .then(({ user }) => {
+        const newUser = new Usuario(user?.uid, nombre, user?.email);
+        return this.firestore.doc(`${user?.uid}/usuario`).set({ ...newUser }); // Usamos la destructuración de newUser para no enviar la instancia de la clase
+      });
   }
 
   loginUsuario(email: string, password: string) {
